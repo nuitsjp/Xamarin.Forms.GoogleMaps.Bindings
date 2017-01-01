da38120efda99eee8639caf9e0948dca5d32b0e2
nuits.jp <nuits.jp@live.jp>
2017-01-01 17:01:58 +0900

    Add EventToCommandBehaviorBase.

---
+++ Soruce/Xamarin.Forms.GoogleMaps.Bindings/EventToCommandBehaviorBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Xamarin.Forms.GoogleMaps.Bindings
+{
+    public abstract class EventToCommandBehaviorBase : BehaviorBase<Map>
+    {
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(ICommand), typeof(MapClickedToCommandBehavior), default(ICommand));
+
+        protected ICommand Commmand
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+    }
+}
--- Soruce/Xamarin.Forms.GoogleMaps.Bindings/MapClickedToCommandBehavior.cs
+++ Soruce/Xamarin.Forms.GoogleMaps.Bindings/MapClickedToCommandBehavior.cs
@@ -7,16 +7,8 @@ using System.Windows.Input;
 
 namespace Xamarin.Forms.GoogleMaps.Bindings
 {
-    public class MapClickedToCommandBehavior : BehaviorBase<Map>
+    public class MapClickedToCommandBehavior : EventToCommandBehaviorBase
     {
-        public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(ICommand), typeof(MapClickedToCommandBehavior), default(ICommand));
-
-        private ICommand Commmand
-        {
-            get { return (ICommand)GetValue(CommandProperty); }
-            set { SetValue(CommandProperty, value);}
-        }
-
         protected override void OnAttachedTo(Map bindable)
         {
             base.OnAttachedTo(bindable);
