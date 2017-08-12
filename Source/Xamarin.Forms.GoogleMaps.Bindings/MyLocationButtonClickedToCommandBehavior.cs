84405610add750c17417052b4cbcef2b2c2665b1
Atsushi Nakamura <nuits.jp@live.jp>
2017-08-12 22:36:12 +0900

    Add MyLocationButtonClickedToCommandBehavior.

---
+++ Source/Xamarin.Forms.GoogleMaps.Bindings/MyLocationButtonClickedToCommandBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.GoogleMaps.Bindings
+{
+    [Preserve(AllMembers = true)]
+
+    public sealed class MyLocationButtonClickedToCommandBehavior : EventToCommandBehaviorBase
+    {
+        protected override void OnAttachedTo(Map bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.MyLocationButtonClicked +=OnMyLocationButtonClicked;
+        }
+
+        protected override void OnDetachingFrom(Map bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.MyLocationButtonClicked -= OnMyLocationButtonClicked;
+        }
+
+        private void OnMyLocationButtonClicked(object sender, MyLocationButtonClickedEventArgs myLocationButtonClickedEventArgs)
+        {
+            Command?.Execute(myLocationButtonClickedEventArgs);
+        }
+    }
+}
