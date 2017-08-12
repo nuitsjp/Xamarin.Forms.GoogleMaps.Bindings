02c0198b06289339159218964ab7ac43d860bf92
Atsushi Nakamura <nuits.jp@live.jp>
2017-08-12 16:31:05 +0900

    Update Packages.

--- Sample/GoogleMaps.Bindings/GoogleMaps.Bindings.iOS/Secret.cs
+++ Sample/GoogleMaps.Bindings/GoogleMaps.Bindings.iOS/Secret.cs
@@ -2,6 +2,6 @@ namespace GoogleMaps.Bindings.iOS
 {
     public static class Secret
     {
-        public const string MapsSdkKey = "API Key";
+        public const string MapsSdkKey = "AIzaSyB0L4hxC7cJT_JjUJR-A_VxonxbC528wsA";
     }
 }
