0e1d72d99e5fc680c97a3bbf434bd7fdb1123999
Ryan Rauch <rauch.ryan@gmail.com>
2018-05-17 07:06:20 -0500

    CameraIdled event
    Updated NuGet packages to Xamarin.Forms 3.0.0.482510, and Xamarin.Forms.GoogleMaps to 3.0.0-beta2
    Added CameraIdled, CameraMoveStarted, CameraMovingTo events

---
+++ Source/Xamarin.Forms.GoogleMaps.Bindings/CameraMoveStartedToCommandBehavior.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms.Internals;
+
+namespace Xamarin.Forms.GoogleMaps.Bindings
+{
+    [Preserve(AllMembers = true)]
+    public sealed class CameraMoveStartedToCommandBehavior : EventToCommandBehaviorBase
+    {
+        protected override void OnAttachedTo(Map bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.CameraMoveStarted += OnCameraMoveStarted;
+        }
+
+        protected override void OnDetachingFrom(Map bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.CameraMoveStarted -= OnCameraMoveStarted;
+        }
+
+        private void OnCameraMoveStarted(object sender, CameraMoveStartedEventArgs cameraMoveStartedEventArgs)
+        {
+            Command?.Execute(cameraMoveStartedEventArgs);
+        }
+    }
+}
